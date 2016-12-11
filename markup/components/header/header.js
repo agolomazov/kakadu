d25fd769c7059fcf52e35d39ee228a03d5841a52
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-12-12 00:48:18 +0300

    Добавлен блок с заголовком страницы

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,21 +1,21 @@
 /*eslint-disable*/
-$(document).ready(function(){
+$(document).ready(function () {
 
     var $menuClone = $('.navigation-menu').clone();
     var $hiddenMenu = $('.hidden-nav');
     var $headerBlock = $('.header');
     var $flagToggle = false;
 
-    (function(){
+    (function () {
         var $headerHeight = $headerBlock.height();
         var $windowHeight = $(window).height();
-        $hiddenMenu.append($menuClone).css('height', $windowHeight - $headerHeight);
+        $hiddenMenu.append($menuClone).css({'height': $windowHeight - $headerHeight + 'px', top: $headerHeight + 'px'});
     })();
 
-    $(document).on('click', '.toggle-nav', function(e){
+    $(document).on('click', '.toggle-nav', function (e) {
         $headerBlock.toggleClass('active');
         $hiddenMenu.toggleClass('active');
-        if(!$flagToggle){
+        if (!$flagToggle) {
             $('body').append('<div class="shadow"></div>');
         } else {
             $('body .shadow').remove();
@@ -24,12 +24,19 @@ $(document).ready(function(){
         $flagToggle = !$flagToggle;
     });
 
-    $(window).on('resize', function(){
+    $(window).on('resize', function () {
+        var $headerHeight = $headerBlock.height();
+        var $windowHeight = $(window).height();
+
         var $windowWidth = $(this).width();
-        if($windowWidth > 940){
+        if ($windowWidth > 940) {
             $headerBlock.removeClass('active');
             $hiddenMenu.removeClass('active');
             $('body .shadow').remove();
+        }
+
+        if ($windowWidth < 940) {
+            $hiddenMenu.css({'height': $windowHeight - $headerHeight + 'px', top: $headerHeight + 'px'});
         }
     });
 
