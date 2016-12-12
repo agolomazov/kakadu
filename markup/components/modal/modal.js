0726b77be854c62a82f7988392601d57afcf160f
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-12-12 03:26:42 +0300

    Добавлен адаптив для модального окна

--- markup/components/modal/modal.js
+++ markup/components/modal/modal.js
@@ -1,1 +1,8 @@
-
+/*eslint-disable*/
+$(document).on('click', '.callback-js', function (e) {
+    $('#modal-js').bPopup({
+        closeClass: 'modal-close',
+        modalColor: 'black'
+    });
+});
+/*eslint-enable*/
