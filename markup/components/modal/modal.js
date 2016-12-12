8c71b3d6db1319be776e62900f873e9e7b0d2853
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-12-12 03:32:38 +0300

    Добавлена маска ввода для модального окна

--- markup/components/modal/modal.js
+++ markup/components/modal/modal.js
@@ -1,8 +1,38 @@
 /*eslint-disable*/
-$(document).on('click', '.callback-js', function (e) {
-    $('#modal-js').bPopup({
-        closeClass: 'modal-close',
-        modalColor: 'black'
+$(document).ready(function(){
+
+    var modalNameInput = document.querySelector('#input-name-js');
+    var superPlaceholderOptions = {
+        // delay between letters (in milliseconds)
+        letterDelay: 100, // milliseconds
+        // delay between sentences (in milliseconds)
+        sentenceDelay: 1000,
+        // should start on input focus. Set false to autostart
+        startOnFocus: true,
+        // loop through passed sentences
+        loop: true,
+        // Initially shuffle the passed sentences
+        shuffle: false,
+        // Show cursor or not. Shows by default
+        showCursor: true,
+        // String to show as cursor
+        cursor: '|'
+    };
+
+    superplaceholder({
+        el: modalNameInput,
+        sentences: ['Введите Ваше имя', 'Например: Владимир'],
+        options: superPlaceholderOptions
+    });
+
+    $('#input-phone-js').inputmask("+7(999)999-99-99");
+
+    $(document).on('click', '.callback-js', function (e) {
+        $('#modal-js').bPopup({
+            closeClass: 'modal-close',
+            modalColor: 'black'
+        });
     });
 });
+
 /*eslint-enable*/
